3964bbdd7df7cc2766b80264eb4d6fd69e19ff35
zhouzhongqing <zhouzhongqing@qq.com>
2025-07-13 17:42:33 +0800

    feat:序列号，数据源

--- src/main/java/com/zzq/domain/Order.java
+++ src/main/java/com/zzq/domain/Order.java
@@ -4,11 +4,15 @@ import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
+import java.io.Serial;
 import java.io.Serializable;
 
 @TableName(value = "t_order")
 public class Order implements Serializable {
 
+
+    @Serial
+    private static final long serialVersionUID = 8347055723013141158L;
 
     public Order() {
     }
